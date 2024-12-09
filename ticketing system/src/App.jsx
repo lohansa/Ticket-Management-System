87d251f6a7366d78e2629c9944f0a3e06436ff9a
Lohansa Munasinghe <lohansamunasinghe3@gmail.com>
2024-12-09 15:40:11 +0530

    Implemented logging page

--- ticketing system/src/App.jsx
+++ ticketing system/src/App.jsx
@@ -1,28 +1,18 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
+import Register from "./components/Register";
+import VendorDashboard from "./components/VendorDashboard";
+import CustomerDashboard from "./components/CustomerDashboard";
 
-const App = () => {
-    return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route
-                        path="/dashboard"
-                        element={
-                            <ProtectedRoute>
-                                <Dashboard />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Routes>
-            </Router>
-        </AuthProvider>
-    );
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/vendor-dashboard" element={<VendorDashboard />} />
+        <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
--- ticketing system/src/components/CustomerDashboard.jsx
+++ ticketing system/src/components/CustomerDashboard.jsx
@@ -1,11 +1,12 @@
 function CustomerDashboard() {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Customer Dashboard</h1>
-        <p>Explore events and book tickets here!</p>
-      </div>
-    );
-  }
-  
-  export default CustomerDashboard;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Customer Dashboard</h1>
+      <p>Explore events and book tickets here!</p>
+    </div>
+  );
+}
+
+export default CustomerDashboard;
+
   
