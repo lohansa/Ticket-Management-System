4d416441062a13710e7510f688d335baf0ba31c8
Lohansa Munasinghe <lohansamunasinghe3@gmail.com>
2024-12-09 20:59:12 +0530

    Navbar Styling

--- ticketing system/src/components/Navbar.jsx
+++ ticketing system/src/components/Navbar.jsx
@@ -1,32 +1,36 @@
+import './Navbar.css' ;
 import React from "react";
 import { Link } from "react-router-dom";
 import {useAuth}
  from "../context/AuthContext";
+
 const Navbar = () => {
   const { userRole, logout } = useAuth();
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      {userRole === "vendor" ? (
-        <>
-          <Link to="/vendor-dashboard">Vendor Dashboard</Link>
-          <Link to="/vendor-dashboard/add-ticket">Add Ticket</Link>
-          <Link to="/vendor-dashboard/add-event">Add Event</Link>
-        </>
-      ) : userRole === "customer" ? (
-        <>
-          <Link to="/customer-dashboard">Customer Dashboard</Link>
-          <Link to="/customer-dashboard/buy-ticket">Buy Ticket</Link>
-          <Link to="/customer-dashboard/event-details">Event Details</Link>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
-      {userRole && <button onClick={logout}>Logout</button>}
+    <nav className="navbar">
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        {userRole === "vendor" ? (
+          <>
+            <Link to="/vendor-dashboard">Vendor Dashboard</Link>
+            <Link to="/vendor-dashboard/add-ticket">Add Ticket</Link>
+            <Link to="/vendor-dashboard/add-event">Add Event</Link>
+          </>
+        ) : userRole === "customer" ? (
+          <>
+            <Link to="/customer-dashboard">Customer Dashboard</Link>
+            <Link to="/customer-dashboard/buy-ticket">Buy Ticket</Link>
+            <Link to="/customer-dashboard/event-details">Event Details</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+        {userRole && <button onClick={logout}>Logout</button>}
+      </div>
     </nav>
   );
 };
