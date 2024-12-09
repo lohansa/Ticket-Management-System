082e02c2c83e5cd1e80984252542c571e8acaf74
Lohansa Munasinghe <lohansamunasinghe3@gmail.com>
2024-12-09 22:25:01 +0530

    Homepage styled

---
+++ ticketing system/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="hero">
+        <h1>Welcome to Our Event Platform</h1>
+        <p>Explore exciting events and manage tickets effortlessly!</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
